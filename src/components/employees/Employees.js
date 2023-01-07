02429e8ad8dcc150e5105e1ad7051bf43a6aa502
seferoff <seferoff007@gmail.com>
2023-01-07 19:49:38 +0400

    not/ready

--- src/components/employees/Employees.js
+++ src/components/employees/Employees.js
@@ -3,52 +3,76 @@ import "./Employees.css";
 
 const Employees = () => {
   const [user, setUser] = useState([]);
-  // const [employee, setEmployee] = useState();
+  const [employee, setEmployee] = useState([]);
+  const [value, setValue] = useState("");
 
   const fetchData = async () => {
     const response = await fetch(
       "https://5ea5ca472d86f00016b4626d.mockapi.io/brotherhood"
     );
     const data = await response.json();
-    return setUser(data);
+    setUser(data);
+    setEmployee(data)
   };
 
   useEffect(() => {
     fetchData();
   }, []);
 
+  const handleChange = (e) => {
+    setValue(e.target.value);
+    console.log(user);
+    const clone = employee.filter((item) => {
+      if (e.target.value !== "") {
+        return +item.id === +e.target.value;
+      }
+      setEmployee(clone);
+    });
+  };
+
   return (
     <main>
       <h1>Employee List</h1>
       <table>
-      <thead>
-        <tr>
-          <th>Id<br></br>
-            <input type="number" placeholder="Search"/></th>
-          <th>
-            Name<br></br>
-            <input type="text" placeholder="Search"/>
-          </th>
-          <th>Department<br></br>
-            <input type="text" placeholder="Search"/></th>
-          <th>Role<br></br>
-            <input type="text" placeholder="Search"/></th>
-        </tr>
+        <thead>
+          <tr>
+            <th>
+              Id<br></br>
+              <input
+                type="number"
+                placeholder="Search"
+                value={value}
+                onChange={(e) => handleChange(e)}
+              />
+            </th>
+            <th>
+              Name<br></br>
+              <input type="text" placeholder="Search" />
+            </th>
+            <th>
+              Department<br></br>
+              <input type="text" placeholder="Search" />
+            </th>
+            <th>
+              Role<br></br>
+              <input type="text" placeholder="Search" />
+            </th>
+          </tr>
         </thead>
         <tbody>
-        {user &&
-          user.length > 0 &&
-          user.map((employee, index) => {
-            return (
-              <tr key={index}>
-                <td>{employee.id}</td>
-                <td>{employee.name}</td>
-                <td>{employee.department}</td>
-                <td>{employee.role}</td>
-              </tr>
-            );
-          })}
-          </tbody>
+          {employee &&
+            employee.length > 0 &&
+            employee.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td>{item.department}</td>
+                  <td>{item.role}</td>
+                </tr>
+              );
+            })}
+        </tbody>
       </table>
     </main>
   );
